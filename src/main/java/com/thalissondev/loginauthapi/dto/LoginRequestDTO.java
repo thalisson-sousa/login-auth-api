2215651778aae8c21bf2d46f5ac5b31368d1b3fd
Thalisson <thalissonsousa33@gmail.com>
2024-03-31 21:16:56 -0300

    create DTO's

---
+++ src/main/java/com/thalissondev/loginauthapi/dto/LoginRequestDTO.java
@@ -0,0 +1,4 @@
+package com.thalissondev.loginauthapi.dto;
+
+public record LoginRequestDTO(String email, String password) {
+}
---
+++ src/main/java/com/thalissondev/loginauthapi/dto/ResponseDTO.java
@@ -0,0 +1,4 @@
+package com.thalissondev.loginauthapi.dto;
+
+public record ResponseDTO(String name, String token) {
+}
